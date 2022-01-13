a75e7ac5794860aa5965fb2c13afdfadbff5738e
Jhon <jmoreno1@iesebre.com>
2022-01-13 18:58:17 +0100

    Loading States Skeleton test al initial loading

--- src/views/Videos.vue
+++ src/views/Videos.vue
@@ -1,65 +1,173 @@
 <template>
-  <ion-list>
-    <ion-list-header> Recent Conversations </ion-list-header>
+  <ion-content>
+    <ion-refresher slot="fixed" @ion-Refresh="refresh" id="refresher">
+      <ion-refresher-content></ion-refresher-content>
+    </ion-refresher>
+    <ion-list>
+      <ion-list-header>
+        Llistat de Videos
+      </ion-list-header>
 
-    <ion-item v-for="video in videos" :key="video.id">
-      <ion-avatar slot="start">
-        <img src="https://via.placeholder.com/80" />
-      </ion-avatar>
-      <ion-label>
-        <h2>{{video.title}}</h2>
-        <p>{{video.description}}</p>
-      </ion-label>
-    </ion-item>
+      <ion-item v-for="video in videos" :key="video.id">
+        <ion-avatar slot="start">
+          <img src="https://via.placeholder.com/80">
+        </ion-avatar>
+        <ion-label>
+          <h2>{{ video.title }}</h2>
+          <p>{{ video.description }}</p>
+        </ion-label>
+      </ion-item>
 
-  </ion-list>
+    </ion-list>
+    <ion-list id="skeleton" v-if="loading">
+      <ion-list-header>
+        <ion-skeleton-text animated style="width: 80px"></ion-skeleton-text>
+      </ion-list-header>
+      <ion-item>
+        <ion-avatar slot="start">
+          <ion-skeleton-text></ion-skeleton-text>
+        </ion-avatar>
+        <ion-label>
+          <h3>
+            <ion-skeleton-text animated style="width: 80%"></ion-skeleton-text>
+          </h3>
+          <p>
+            <ion-skeleton-text animated style="width: 60%"></ion-skeleton-text>
+          </p>
+        </ion-label>
+      </ion-item>
+      <ion-item>
+        <ion-avatar slot="start">
+          <ion-skeleton-text></ion-skeleton-text>
+        </ion-avatar>
+        <ion-label>
+          <h3>
+            <ion-skeleton-text animated style="width: 80%"></ion-skeleton-text>
+          </h3>
+          <p>
+            <ion-skeleton-text animated style="width: 60%"></ion-skeleton-text>
+          </p>
+        </ion-label>
+      </ion-item>
+      <ion-item>
+        <ion-avatar slot="start">
+          <ion-skeleton-text></ion-skeleton-text>
+        </ion-avatar>
+        <ion-label>
+          <h3>
+            <ion-skeleton-text animated style="width: 80%"></ion-skeleton-text>
+          </h3>
+          <p>
+            <ion-skeleton-text animated style="width: 60%"></ion-skeleton-text>
+          </p>
+        </ion-label>
+      </ion-item>
+      <ion-item>
+        <ion-avatar slot="start">
+          <ion-skeleton-text></ion-skeleton-text>
+        </ion-avatar>
+        <ion-label>
+          <h3>
+            <ion-skeleton-text animated style="width: 80%"></ion-skeleton-text>
+          </h3>
+          <p>
+            <ion-skeleton-text animated style="width: 60%"></ion-skeleton-text>
+          </p>
+        </ion-label>
+      </ion-item>
+      <ion-item>
+        <ion-avatar slot="start">
+          <ion-skeleton-text></ion-skeleton-text>
+        </ion-avatar>
+        <ion-label>
+          <h3>
+            <ion-skeleton-text animated style="width: 80%"></ion-skeleton-text>
+          </h3>
+          <p>
+            <ion-skeleton-text animated style="width: 60%"></ion-skeleton-text>
+          </p>
+        </ion-label>
+      </ion-item>
+      <ion-item>
+        <ion-avatar slot="start">
+          <ion-skeleton-text></ion-skeleton-text>
+        </ion-avatar>
+        <ion-label>
+          <h3>
+            <ion-skeleton-text animated style="width: 80%"></ion-skeleton-text>
+          </h3>
+          <p>
+            <ion-skeleton-text animated style="width: 60%"></ion-skeleton-text>
+          </p>
+        </ion-label>
+      </ion-item>
+      <ion-item>
+        <ion-avatar slot="start">
+          <ion-skeleton-text></ion-skeleton-text>
+        </ion-avatar>
+        <ion-label>
+          <h3>
+            <ion-skeleton-text animated style="width: 80%"></ion-skeleton-text>
+          </h3>
+          <p>
+            <ion-skeleton-text animated style="width: 60%"></ion-skeleton-text>
+          </p>
+        </ion-label>
+      </ion-item>
+    </ion-list>
+  </ion-content>
 </template>
 
 <script>
-import {IonAvatar, IonItem, IonLabel, IonList, IonListHeader} from "@ionic/vue";
-import axios from "axios";
-import casteaching from '@acacha/casteaching'
-
-
+import casteaching from "@acacha/casteaching";
+import {
+  IonAvatar,
+  IonContent,
+  IonItem,
+  IonLabel,
+  IonList,
+  IonListHeader,
+  IonRefresher,
+  IonRefresherContent, IonSkeletonText
+} from "@ionic/vue";
 export default {
-  name: 'Videos',
-  components:{
+  name: "Videos",
+  data(){
+    return{
+      videos:[],
+      loading: false
+    }
+  },
+  components: {
     IonList,
     IonItem,
     IonListHeader,
     IonAvatar,
-    IonLabel
-
+    IonLabel,
+    IonRefresher,
+    IonRefresherContent,
+    IonContent,
+    IonSkeletonText
   },
-  data(){
-    return{
-      videos: [
-        {
-            id: 1,
-            title: "Ubuntu 101",
-            description: "Here description",
-            url: "https://www.youtube.com/embed/w8j07_DBl_I",
-        }, {
-            id: 2,
-            title: "Ubuntu 102",
-            description: "Here description",
-            url: "https://www.youtube.com/embed/w8j07_DBl_I",
-        }, {
-            id: 3,
-            title: "Ubuntu 103",
-            description: "Here description",
-            url: "https://www.youtube.com/embed/w8j07_DBl_I",
-        },
-      ]
+  methods:{
+    async refresh(){
+      await this.fetchVideos()
+      this.refresher.complete()
+    },
+    async fetchVideos(){
+      this.loading = true
+      this.videos = await casteaching.videos()
+      this.loading = false
     }
   },
   async created() {
-    this.videos= await casteaching.videos()
-
-
+    await this.fetchVideos()
+  },
+  mounted() {
+    this.refresher = document.getElementById('refresher')
+  }
 }
 </script>
 
 <style scoped>
-
 </style>
